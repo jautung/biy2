4f948231095e7a65ab22e34790939ac5b41fd4e9
Jau Tung Chan <chanjautung212@gmail.com>
2023-10-11 22:37:01 -0700

    reasonable placeholder grid

--- level.py
+++ level.py
@@ -5,6 +5,10 @@ from window import Window
 from map import Map
 
 
+COLOR_WHITE = (1.0, 1.0, 1.0)
+COLOR_GRAY = (0.4, 0.4, 0.4)
+
+
 class Level:
     def __init__(self, title: str, window: Window, map: Map):
         self.title = title
@@ -33,40 +37,54 @@ class Level:
 
 
     def _display_func(self):
-        self._display_reset()
+        self._gl_display_reset()
         self._display_grid()
         self._display_map()
-        self._display_flush()
-        glutSwapBuffers()
+        self._gl_display_commit()
 
 
-    def _display_reset(self):
+    def _gl_display_reset(self):
         glClear(GL_COLOR_BUFFER_BIT)
 
 
     def _display_grid(self):
-        # TODO
-        glColor3f(0.0, 1.0, 0.0)
-        glBegin(GL_LINES)
-        glVertex2f(0.0, 0.0)
-        glVertex2f(1.0, 1.0)
-        glEnd()
+        # TODO: Use map number of rows and columns correctly
+        self._gl_display_line(color=COLOR_GRAY, x0=-1, y0=0, x1=1, y1=0)
+        self._gl_display_line(color=COLOR_GRAY, x0=-1, y0=0.5, x1=1, y1=0.5)
+        self._gl_display_line(color=COLOR_GRAY, x0=-1, y0=-0.5, x1=1, y1=-0.5)
+        self._gl_display_line(color=COLOR_GRAY, x0=0, y0=-1, x1=0, y1=1)
+        self._gl_display_line(color=COLOR_GRAY, x0=0.5, y0=-1, x1=0.5, y1=1)
+        self._gl_display_line(color=COLOR_GRAY, x0=-0.5, y0=-1, x1=-0.5, y1=1)
 
 
     def _display_map(self):
-        # TODO
-        glColor3f(0.0, 1.0, 0.0)
+        # TODO: Display map and pieces!
+        self._gl_display_point(color=COLOR_WHITE, size=30, x=self._temp_x, y=self._temp_y)
+
+
+    def _gl_display_commit(self):
+        glFlush()
+        glutSwapBuffers()
+
+
+    def _gl_display_point(self, color, size, x, y):
+        glColor3f(*color)
+        glPointSize(size)
         glBegin(GL_POINTS)
-        glVertex2f(self._temp_x, self._temp_y)
+        glVertex2f(x, y)
         glEnd()
 
 
-    def _display_flush(self):
-        glFlush()
+    def _gl_display_line(self, color, x0, y0, x1, y1):
+        glColor3f(*color)
+        glBegin(GL_LINES)
+        glVertex2f(x0, y0)
+        glVertex2f(x1, y1)
+        glEnd()
 
 
     def _keyboard_func(self, key, x, y):
-        # TODO
+        # TODO: Actually implement logic
         if key == b'w':
             self._temp_y += 0.1
         elif key == b'a':
