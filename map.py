7f51e16b8a72cd0f75faed2c1d26913cfc0a0880
Jau Tung Chan <chanjautung212@gmail.com>
2023-10-15 08:51:18 -0700

    rule generation not working

--- map.py
+++ map.py
@@ -1,5 +1,8 @@
+import copy
+
 from mappiece import MapPiece
 from movedirection import MoveDirection
+from piecetype import PieceType, WordNounPieceType, WordIsPieceType, WordAttributePieceType, BabaObjectPieceType, FlagObjectPieceType, BabaWordPieceType, FlagWordPieceType, WinWordPieceType, YouWordPieceType
 
 
 class Map:
@@ -32,13 +35,37 @@ class Map:
         return pieces
 
 
-    def generate_rules(self):
-        # TODO
-        return []
+    def _generate_rules_for_grid(self):
+        row_rules = [self._generate_rules_for_row(row) for row in self.grid]
+        column_rules = [self._generate_rules_for_row([self.grid[row_index][column_index] for row_index in range(self.number_rows)]) for column_index in range(self.number_columns)]
+        print(row_rules + column_rules)
+        return row_rules + column_rules
+
+
+    def _generate_rules_for_row(self, row: list[PieceType]):
+        rules = []
+        for start_index in range(len(row)):
+            length = self._generate_logest_possible_rule_given_start(row[start_index:])
+            if length is None:
+                continue
+            rules.append(copy.deepcopy(row[start_index:start_index+length]))
+        return rules
+
+
+    def _generate_logest_possible_rule_given_start(self, row: list[PieceType]):
+        # TODO: Incorporate 'NOT', 'AND', etc. etc.
+        if row[0] is not WordNounPieceType:
+            return None
+        if len(row) <= 1 or row[1] is not WordIsPieceType:
+            return None
+        if len(row) <= 2 or row[2] is not WordAttributePieceType:
+            return None
+        return 3
 
 
     def execute_move(self, direction: MoveDirection):
         # TODO: Only affect pieces that are you, based on rules
+        rules = self._generate_rules_for_grid()
         for piece in self.get_pieces():
             if direction == MoveDirection.UP:
                 piece.y += 1
